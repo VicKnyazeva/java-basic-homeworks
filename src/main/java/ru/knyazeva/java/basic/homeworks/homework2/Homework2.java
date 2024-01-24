75fe76411954b1237ebbee6be2c6ae9c68a142c2
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-01-24 17:51:40 +0700

    Homework #2: небольшие правки

--- src/main/java/ru/knyazeva/java/basic/homeworks/homework2/Homework2.java
+++ src/main/java/ru/knyazeva/java/basic/homeworks/homework2/Homework2.java
@@ -6,29 +6,58 @@ public class Homework2 {
     public static void main(String[] args) {
 
         //Реализуйте метод, принимающий в качестве аргументов целое число и строку, и печатающий в консоль строку указанное количество раз
-        //printString(5, "Hello");
+        System.out.println("Метод 1");
+        System.out.println("-------");
+        printString(3, "Hello");
+        System.out.println("-------");
+        System.out.println();
 
         //Реализуйте метод, принимающий в качестве аргумента целочисленный массив, суммирующий все элементы, значение которых больше 5, и печатающий полученную сумму в консоль.
-        int[] arr = {1, 2, 6, 5, 7, 0, 8, 9};
-        //sumArrayElements(arr);
+        System.out.println("Метод 2");
+        System.out.println("-------");
+        int[] arr = {1, 2, 6, 5, 7, 0, 8, 0};
+        System.out.println(Arrays.toString(arr));
+        sumArrayElements(arr);
+        System.out.println("-------");
+        System.out.println();
 
         // Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на целочисленный массив,
         // метод должен заполниться каждую ячейку массива указанным числом.
+        System.out.println("Метод 3");
+        System.out.println("-------");
         int[] arr1 = new int[10];
-        //fillArray(5, arr1);
-        //System.out.printlnArrays.toString(arr1));
+        fillArray(5, arr1);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println("-------");
+        System.out.println();
 
         // Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на целочисленный массив,
         // увеличивающий каждый элемент которого на указанное число.
         int[] arr2 = new int[10];
-        //fillArray(42, arr2);
-        //increaseArrayElements(3, arr2);
+        System.out.println("Метод 4");
+        System.out.println("-------");
+
+        fillArray(42, arr2);
+        System.out.println(Arrays.toString(arr2));
+
+        increaseArrayElements(3, arr2);
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println("-------");
+        System.out.println();
 
         // Реализуйте метод, принимающий в качестве аргумента целочисленный массив,
         // и печатающий в консоль сумма элементов какой из половин массива больше.
+        System.out.println("Метод 5");
+        System.out.println("-------");
+
         int[] arr3 = {1, 2, 1, 1, 1};
         System.out.println(Arrays.toString(arr3));
-        printGreaterPart(arr3);
+
+        printSumGreaterPart(arr3);
+
+
+        System.out.println("-------");
     }
 
     // Реализуйте метод, принимающий в качестве аргументов целое число и строку,
@@ -68,7 +97,7 @@ public class Homework2 {
 
     // Реализуйте метод, принимающий в качестве аргумента целочисленный массив,
     // и печатающий в консоль сумма элементов какой из половин массива больше.
-    private static void printGreaterPart(int[] arr) {
+    private static void printSumGreaterPart(int[] arr) {
         int sumLeft = 0;
         int sumRight = 0;
         for (int i = 0; i < arr.length/2; i++) {
