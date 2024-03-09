cf20662d5c670f2ded1457e0a4a6133fe005dbcd
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-03-10 01:55:30 +0700

    Homework #9 - fixed

--- src/main/java/ru/knyazeva/java/basic/homeworks/homework9/Application.java
+++ src/main/java/ru/knyazeva/java/basic/homeworks/homework9/Application.java
@@ -8,43 +8,50 @@ import static java.lang.System.out;
 
 public class Application {
     public static void main(String[] args) {
-        ArrayList<Integer> arrList = getMinMaxDiapason(1, 5);
-        out.println(arrList);
+        try {
+            List<Integer> arrList = getMinMaxDiapason(1, 6);
+            out.println(arrList);
 
-        out.println("Sum of list elements greater than 5: " + sumListElements(arrList));
+            out.println("Sum of list elements greater than 5: " + sumListElements(arrList));
 
-        overwriteListElements(153, arrList);
-        out.println(arrList);
+            overwriteListElements(153, arrList);
+            out.println(arrList);
 
-        increaseListElements(5, arrList);
-        out.println(arrList);
+            increaseListElements(5, arrList);
+            out.println(arrList);
 
-        ArrayList<Employee> employeesList = new ArrayList<>();
-        employeesList.add(new Employee("Ivan", 35));
-        employeesList.add(new Employee("Nikita", 21));
-        employeesList.add(new Employee("Olga", 33));
-        employeesList.add(new Employee("Oleg", 36));
-        out.println(getEmployeesNames(employeesList));
-        out.println(getGreaterThanMinAgeEmployees(employeesList, 33));
-        out.println(getMinAgeEmployee(employeesList));
-        out.println(isMiddleAge(employeesList, 29));
+            ArrayList<Employee> employeesList = new ArrayList<>();
+            employeesList.add(new Employee("Ivan", 35));
+            employeesList.add(new Employee("Nikita", 21));
+            employeesList.add(new Employee("Olga", 33));
+            employeesList.add(new Employee("Oleg", 36));
+            out.println(getEmployeesNames(employeesList));
+            out.println(getGreaterThanMinAgeEmployees(employeesList, 33));
+            out.println(getMinAgeEmployee(employeesList));
+            out.println(isMiddleAge(employeesList, 29));
+        } catch (Exception ex) {
+            out.println(ex.getMessage());
+        }
     }
 
-    public static ArrayList<Integer> getMinMaxDiapason(int min, int max) {
-        ArrayList<Integer> result = new ArrayList<>(max - min + 1);
-        if (max > min) {
-            for (int i = min; i <= max; i++) {
-                result.add(i);
-            }
+    public static List<Integer> getMinMaxDiapason(int min, int max) throws CustomListException {
+        if (max < min)
+            throw new CustomListException("Max value can not be less than Min value");
+
+        List<Integer> result = new ArrayList<>(max - min + 1);
+        for (int i = min; i <= max; i++) {
+            result.add(i);
         }
         return result;
     }
 
     public static int sumListElements(List<Integer> list) {
         int sum = 0;
-        for (Integer elem : list) {
-            if (elem > 5)
-                sum += elem;
+        if (list != null) {
+            for (Integer elem : list) {
+                if (elem > 5)
+                    sum += elem;
+            }
         }
         return sum;
     }
@@ -52,7 +59,7 @@ public class Application {
     public static void overwriteListElements(int num, List<Integer> list) {
         if (list != null) {
             for (int i = 0; i < list.size(); i++) {
-                list.set(i, Integer.valueOf(num));
+                list.set(i, num);
             }
         }
     }
@@ -61,29 +68,36 @@ public class Application {
         if (list != null) {
             for (int i = 0; i < list.size(); i++) {
                 int element = list.get(i);
-                list.set(i, Integer.valueOf(element + num));
+                list.set(i, element + num);
             }
         }
     }
 
-    public static ArrayList<String> getEmployeesNames(List<Employee> employees) {
-        ArrayList<String> names = new ArrayList<>();
-        for (Employee e : employees) {
-            names.add(e.getName());
+    public static List<String> getEmployeesNames(List<Employee> employees) {
+        List<String> names = new ArrayList<>();
+        if (employees != null) {
+            for (Employee e : employees) {
+                names.add(e.getName());
+            }
         }
         return names;
     }
 
-    public static ArrayList<Employee> getGreaterThanMinAgeEmployees(List<Employee> employees, int minAge) {
-        ArrayList<Employee> result = new ArrayList<>();
-        for (Employee e : employees) {
-            if (e.getAge() >= minAge)
-                result.add(e);
+    public static List<Employee> getGreaterThanMinAgeEmployees(List<Employee> employees, int minAge) {
+        List<Employee> result = new ArrayList<>();
+        if (employees != null) {
+            for (Employee e : employees) {
+                if (e.getAge() >= minAge)
+                    result.add(e);
+            }
         }
         return result;
     }
 
-    public static boolean isMiddleAge(List<Employee> employees, int minMiddleAge) {
+    public static boolean isMiddleAge(List<Employee> employees, int minMiddleAge) throws CustomListException {
+        if (employees == null || employees.isEmpty())
+            throw new CustomListException("List parameter can not be null or empty");
+
         int midAge = 0;
         for (Employee e : employees) {
             midAge += e.getAge();
@@ -93,14 +107,11 @@ public class Application {
     }
 
     public static Employee getMinAgeEmployee(List<Employee> employees) {
-        int minAge = Integer.MAX_VALUE;
         Employee employee = null;
-
-        for (Employee e : employees) {
-            int currentAge = e.getAge();
-            if (currentAge < minAge) {
-                minAge = currentAge;
-                employee = e;
+        if (employees != null) {
+            for (Employee e : employees) {
+                if (employee == null || (e.getAge() < employee.getAge()))
+                    employee = e;
             }
         }
         return employee;
---
+++ src/main/java/ru/knyazeva/java/basic/homeworks/homework9/CustomListException.java
@@ -0,0 +1,7 @@
+package ru.knyazeva.java.basic.homeworks.homework9;
+
+public class CustomListException extends Exception {
+    public CustomListException(String message) {
+        super(message);
+    }
+}
