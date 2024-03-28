08226828e7dba1c1b8bad03dd3c53d0e2da0eb37
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-03-29 02:10:21 +0700

    Homework #13

--- src/main/java/ru/knyazeva/java/basic/homeworks/homework13/ClientApp.java
+++ src/main/java/ru/knyazeva/java/basic/homeworks/homework13/ClientApp.java
@@ -5,23 +5,27 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         try (Socket socket = new Socket("localhost", 8080)) {
             ClientExample client = new ClientExample(socket.getInputStream(), socket.getOutputStream());
+
+            var reply = client.receive();
+            System.out.println("На сервере доступны операции: " + reply);
+
             while (true) {
-                System.out.println("Enter message:");
+                System.out.print("Введите операцию и операнды через пробел или exit для выхода: ");
                 String userMessage = scanner.nextLine();
                 if (userMessage.equals("exit")) {
                     break;
                 }
                 client.send(userMessage);
 
-                var reply = client.receive();
-                System.out.println(reply);
+                reply = client.receive();
+                System.out.println("Ответ сервера: " + reply);
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IOException(e);
         }
     }
 }
